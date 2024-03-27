f14cb6a762dc9761be5427587c6f8534880134f1
Legend <me@liux.pro>
2024-03-27 13:45:25 +0800

    web flash tool

---
+++ flash-online/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <tiny-tabs v-model="activeName">
+    <tiny-tab-item title="烧录" name="flash">
+      <Flash />
+    </tiny-tab-item>
+    <tiny-tab-item title="远程控制" name="control">
+      <Control />
+    </tiny-tab-item>
+  </tiny-tabs>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Tabs as TinyTabs, TabItem as TinyTabItem } from '@opentiny/vue'
+import AboutView from '@/views/AboutView.vue'
+import Flash from '@/views/Flash.vue'
+import Control from '@/views/Control.vue'
+
+const activeName = ref('control')
+</script>
+
+<style>
+.pc-demo .tiny-tabs__content {
+  line-height: 1.5;
+}
+</style>
